78956636bcea07febb9706643767d9b4aaa8fb85
Marko <maxmagnus14@yahoo.com>
2012-10-02 15:01:58 +0200

    Added monthly based calendar reminder

--- src/com/ppero196/thumbnail/file/deleter/MainActivity.java
+++ src/com/ppero196/thumbnail/file/deleter/MainActivity.java
@@ -1,13 +1,17 @@
 package com.ppero196.thumbnail.file.deleter;
 
 import java.io.File;
+import java.util.Calendar;
 
 import android.os.Bundle;
 import android.os.Environment;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -33,5 +37,27 @@ public class MainActivity extends Activity {
         }
     	Toast.makeText(getApplicationContext(), 
                 "Thumbnail files deleted!", Toast.LENGTH_LONG).show();
+    }
+    public void reminder(View view) {
+    	Calendar cal = Calendar.getInstance();              
+    	Intent intent = new Intent(Intent.ACTION_EDIT);
+    	intent.setType("vnd.android.cursor.item/event");
+    	intent.putExtra("beginTime", cal.getTimeInMillis());
+    	intent.putExtra("allDay", true);
+    	intent.putExtra("rrule", "FREQ=MONTHLY");
+    	intent.putExtra("endTime", cal.getTimeInMillis()+60*60*1000);
+    	intent.putExtra("title", "Clear thumbnails!!!");
+    	startActivity(intent);
+    }
+    
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.Credits:
+            	WebView webview = new WebView(this);
+              	 setContentView(webview);
+              	 webview.loadUrl("file:///android_res/raw/credits.html");
+                return true;
+        }
+		return true;
     }
 }
